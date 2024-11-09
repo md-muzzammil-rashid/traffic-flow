9ac65c0bf93756ed32398ee8d2d0b19e4c655350
MdMuzzammilRashid <ayaanrashid960@gmail.com>
2024-11-10 02:00:11 +0530

    configuration

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,59 @@
 package main
 
+import (
+	"context"
+	"fmt"
+	"log"
+	"log/slog"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/gorilla/mux"
+	"github.com/md-muzzammil-rashid/traffic-flow/config"
+)
+
 func main() {
+	cfg, err:= config.InitConfig()
+	if err!= nil {
+        fmt.Println("Error initializing config:", err)
+        return
+    }
+
+	router := mux.NewRouter()
+
+	server := http.Server{
+		Handler: router,	
+		Addr: fmt.Sprintf("%s:%d", cfg.Host, cfg.Port),
+	}
+
+	done := make(chan os.Signal, 1)
+
+	signal.Notify(done, syscall.SIGINT, syscall.SIGTERM)
+	go func ()  {
+		slog.Info("Server is running on PORT: ",cfg.Port)
+		err = server.ListenAndServe(); if err != nil {
+			log.Fatalf("Error: failed to start the server : %s", err.Error())
+		}
+	}()
+
+	<- done
+
+	log.Println("Shutting down the server...")
 	
+	ctx, cancle := context.WithTimeout(context.Background(), time.Second*30)
+
+	defer cancle()
+
+	err = server.Shutdown(ctx); if err != nil {
+		log.Fatalf("Error: failed to gracefully shutdown the server : %s", err.Error())
+	}
+
+	slog.Info("Server shutdown completed")
+
+
+
+
 }
--- config/config.go
+++ config/config.go
@@ -1,9 +1,45 @@
 package config
 
+import (
+	"flag"
+	"log"
+	"os"
+
+	"github.com/ilyakaznacheev/cleanenv"
+)
+
 type Config struct {
 	Port int `env:"PORT"`
 	Host string `env:"HOST"`
 	TotalServers int `env:"TOTAL_SERVERS"`
+
 	
+}
+
+func InitConfig() (Config, error)  {
+	configurationPath := os.Getenv("CONFIG_PATH")
+	if configurationPath == "" {
+		flags := flag.String("config", "", "path to the configuration file")
+		flag.Parse()
+		configurationPath = *flags
+
+		if configurationPath == "" {
+            log.Fatal("Error: configuration path is not provided")
+        }
+	}
+
+	_, err := os.Stat(configurationPath); if os.IsNotExist(err) {
+		log.Fatalf("Error: configuration file at %s not exist!", configurationPath)
+	}
+
+	var config Config
+
+	err = cleanenv.ReadConfig(configurationPath, config); if err != nil {
+		log.Fatal("Error: Failed to read configuration file")
+	}
+
+	cleanenv.ReadEnv(config)
+
+	return config, nil
 }
 
